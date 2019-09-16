737671531b5cba5f1d05368dbde4892b67e88986
Tiago Almeida <tm.almeida@ua.pt>
2019-09-17 00:20:18 +0100

    histogram_incomplete

--- advanced_lane_detection/src/lane_detection_node.py
+++ advanced_lane_detection/src/lane_detection_node.py
@@ -35,6 +35,29 @@ class image_converter:
         image_processed=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         image_processed=cv2.Canny(image_processed,100,150,3)
         image_processed=cv2.warpPerspective(image_processed,perspective_matrix,(image.shape[1],image.shape[0]))
+        image_processed=cv2.inRange(image_processed,1,255)
+
+        # Split the initial image into different channels
+        image_bin=cv2.warpPerspective(image,perspective_matrix,(image.shape[1],image.shape[0]))
+        blue,green,red=cv2.split(image_bin)
+
+        # Red channel binarization
+        image_bin=cv2.inRange(red,200,255)
+
+        # Merge the binarized red channel with the edge detected image
+        image_final = cv2.add(image_processed,image_bin)
+        image_finalRGB=cv2.cvtColor(image_final,cv2.COLOR_GRAY2RGB)
+
+        # Calculate the histogram
+        self.calHist(image_final)
+
+
+        # Detect the lane boundary
+
+
+
+        # Lane curve fitting
+
         
 
 
@@ -45,13 +68,28 @@ class image_converter:
         if cols>60 and rows>60:
             cv2.circle(image,(50,50),10,255)
         cv2.imshow("Initial Image",image)
-        cv2.imshow("Processed Image",image_processed)
+        cv2.imshow("Edge Image Warpped",image_processed)
+        cv2.imshow("Initial Warpped Image Binarized",image_bin)
+        cv2.imshow("Final Image",image_finalRGB)
         cv2.waitKey(3)
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(image_processed,"mono8"))
         except CvBridgeError as e:
             print(e)
+        
+    def calHist(self,im):
+        histogram=[]
+        mid_height=im.shape[0]*0.55
+        mid_height=int(mid_height)
+        for i in range(im.shape[1]): #Width
+            ROI = im[i:(im.shape[0]-mid_height - 1),1:mid_height]
+            dst=cv2.divide(255,ROI)
+            # print(np.sum(dst[0]))
+
+            # histogram.append(np.sum(dst[0]))
+
+
 
 
 
